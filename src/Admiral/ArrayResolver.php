387e8432f954e9a714a222985c56984f98ae1d59
Keith Mifsud <mifsud.k@gmail.com>
2018-01-22 03:10:05 +0000

    GH-3 Added example commands and handlers for tetsing.

---
+++ src/Admiral/ArrayResolver.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of SwellPhp Admiral
+ *
+ * @licence   Please view the Licence file supplied with this source code.
+ *
+ * @author    Keith Mifsud <http://www.keith-mifsud.me>
+ *
+ * @copyright Keith Mifsud 2018 <mifsud.k@gmail.com>
+ *
+ * @since     1.0
+ * @version   1.0 Initial Release
+ */
+
+namespace SwellPhp\Admiral;
+
+/**
+ * Resolves the command handlers and their dependencies
+ * from an array.
+ *
+ */
+final class ArrayResolver implements CommandHandlerResolver
+{
+
+}
---
+++ tests/Admiral/Examples/command_handlers.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of SwellPhp Admiral
+ *
+ * @licence   Please view the Licence file supplied with this source code.
+ *
+ * @author    Keith Mifsud <http://www.keith-mifsud.me>
+ *
+ * @copyright Keith Mifsud 2018 <mifsud.k@gmail.com>
+ *
+ * @since     1.0
+ * @version   1.0 Initial Release
+ */
+
+
+/**
+ * An array of commands with their handlers and dependencies.
+ */
+$commandHandlers = [
+
+];
+
+return $commandHandlers;
+ 
